e8b776acb946be3e8b80748e115ad439f819bb9b
Luke Simpson <luke.simpson@ruralsourcing.com>
2019-11-01 10:51:55 -0500

    show unsplash images

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [photos, setPhotos] = useState(null);
+
+  const onSearchChange = (e) => {
+    let value = e.target.value.replace(/\s/g, '');
+    setSearch(value);
+  }
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    fetchSearchPhotos(search);
+  }
+
+  const fetchSearchPhotos = async (search) => {
+    // setFetching(true);
+    setPhotos(null);
+
+    let res = await fetch(`/unsplash/search/photos?query=${search}`, {
+      method: 'GET',
+    });
+    if (!res) return;
+
+    let json = await res.json();
+    if (!json) return;
+
+    // setFetching(false);
+    setPhotos(json);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <header><h1>Unsplash Client</h1></header>
+      <form onSubmit={onSearchSubmit}>
+        <input class="search" value={search} onChange={onSearchChange}></input>
+      </form>
+      {photos && photos.results.map((photo) => {
+        return <img src={photo.urls.small} />;
+      })}
     </div>
   );
 }
